c595babc2f1a24fdeb62f8a89be4809fc8676684
pzhen <624359088@qq.com>
2019-12-05 23:34:43 +0800

    redis-哨兵模式链接测试

--- src/main/test/Test.java
+++ src/main/test/Test.java
@@ -1,120 +1,62 @@
-import cn.zhen.model.sys.Menu;
+import cn.zhen.mapper.sys.RoleMapper;
+import cn.zhen.model.sys.Role;
+import cn.zhen.utiles.SqlSessionFactoryUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.log4j.Logger;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPoolConfig;
+import redis.clients.jedis.JedisSentinelPool;
 
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
-import java.util.Comparator;
-import java.util.TreeSet;
+import java.util.*;
 
+public class MYTest {
 
+    /*
+        本地开启redis哨兵模式
+     ./redis-server  ../master-6379.conf
+     ./redis-server  ../slave.6380.conf
+     ./redis-server  ../slave.6381.conf
 
-
-
-interface Person {
-    public void buy();
-    public void buy1();
-}
-
-class XiaoQiang implements Person {
-    private String name;
-    private String house;
-
-    public XiaoQiang(String name, String house) {
-        this.name = name;
-        this.house = house;
-    }
-
-    @Override
-    public void buy() {
-        System.out.println(name+"买了"+house);
-    }
-
-    @Override
-    public void buy1() {
-        System.out.println("我是你爸爸");
-    }
-}
-
-class ProxySaler implements InvocationHandler {
-
-    public Object person;
-
-    public Object newInstall(Object person)
-    {
-        this.person=person;
-        return  Proxy.newProxyInstance(person.getClass().getClassLoader(),person.getClass().getInterfaces(),this);
-    }
-
-    @Override
-    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        return method.invoke(person,args);
-    }
-}
-
-
-
-
-
-public class Test {
-
+     ./redis-sentinel ../sentinel.63791.conf
+     ./redis-sentinel ../sentinel.63801.conf
+     ./redis-sentinel ../sentinel.63811.conf
+     */
 
     @org.junit.Test
-    public void test2()
-    {
-        ProxySaler proxySaler=new ProxySaler();
-        Person object= (Person) proxySaler.newInstall(new XiaoQiang("黄豪强","南山区"));
-        object.buy1();
-        object.buy();
+    public  void testSentinel() {
+        // 连接池配置
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+        jedisPoolConfig.setMaxTotal(10);
+        jedisPoolConfig.setMaxIdle(5);
+        jedisPoolConfig.setMinIdle(5);
+        // 哨兵信息
+        Set<String> sentinels = new HashSet<String>(
+                Arrays.asList("127.0.0.1:63791", "127.0.0.1:63801", "127.0.0.1:63811"));
+        // 创建连接池
+        JedisSentinelPool pool = new JedisSentinelPool("mymaster", sentinels, jedisPoolConfig, "grs");
+        // 获取客户端
+        Jedis jedis = pool.getResource();
+
+        // 执行两个命令
+        jedis.set("mykey", "myvalue");
+        String myvalue = jedis.get("mykey");
+        // 打印信息
+        System.out.println(myvalue);
     }
 
     @org.junit.Test
-    public void test4(){
-
-        Long a = System.currentTimeMillis()/1000;
-
-        System.out.println(a);
-
-    }
-
-
-
-    @org.junit.Test
-    public void test1(){
-
-        Menu menu1 = new Menu();
-        menu1.setMenuPath(1);
-
-        Menu menu2 = new Menu();
-        menu2.setMenuPath(2);
-
-        Menu menu3 = new Menu();
-        menu3.setMenuPath(3);
-
-        Menu menu4 = new Menu();
-        menu4.setMenuPath(3);
-
-        TreeSet<Menu> set = new TreeSet<Menu>(new Comparator<Menu>() {
-            @Override
-            public int compare(Menu o1, Menu o2) {
-                if (o1.getMenuPath() > o2.getMenuPath()) {
-                    return 1;
-                }else if (o1.getMenuPath() < o2.getMenuPath()) {
-                    return -1;
-                }else {
-                    return 0;
-                }
+    public void test5(){
+        Logger log = Logger.getLogger(MYTest.class);
+        SqlSession sqlSession = null;
+        try {
+            sqlSession = SqlSessionFactoryUtils.openSqlSession();
+            RoleMapper roleMapper = sqlSession.getMapper(RoleMapper.class);
+            Role role = roleMapper.getRowByID(1);
+            log.info(role.getRoleName());
+        } finally {
+            if (sqlSession != null) {
+                sqlSession.close();
             }
-        });
-        set.add(menu1);
-        set.add(menu2);
-        set.add(menu3);
-        set.add(menu4);
-        System.out.println(set);
-    }
-
-
-    @org.junit.Test
-    public void test3(){
-
+        }
     }
 }
